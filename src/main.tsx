6eab10587c0b6a747dd8784388d044c3bbe2be59
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-11-17 11:23:39 -0300

    feat: Adiciona componentes de login e configuração de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from '../src/App.tsx'
+
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import LoginPage from './pages/LoginPage/index.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {path:"/",element:<LoginPage/>}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/routes/AreaLogin/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function AreaLogin(){
+
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
