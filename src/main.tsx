0bf9db44703b2e95a6d0e0f5415aaba9f7a4a3c1
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-11-18 23:47:38 -0300

    Adiciona rotas para cadastro de pessoas, empresas e acadêmicos

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from '../src/App.tsx'
 
 import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import LoginPage from './pages/LoginPage/index.tsx'
+import CadastroPage from './pages/CadastroPage/index.tsx'
 
 
 const router = createBrowserRouter([
@@ -12,7 +13,27 @@ const router = createBrowserRouter([
     path:"/",
     element:<App/>,
     children:[
-      {path:"/",element:<LoginPage/>}
+      {
+        path:"/login",
+        element:<LoginPage/>
+      },
+      {
+        path:"/cadastro",
+        children:[
+          {
+            path:"/pessoas",
+            element:<CadastroPage />
+          },
+          {
+            path:"/empresas",
+          },
+          {path:"/academico"},
+        ]
+      },
+      {
+        path:"/quemsoueu",
+        element: </> // Componente que retorna quem é a pessoa se cadastrando : pessoa, profissional, inst academica
+      }
     ]
   }
 ])
